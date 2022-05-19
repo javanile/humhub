e67a15fac1aabf8819448dc5c083cdab17b6d488
Francesco Bianco <info.francescobianco@gmail.com>
2022-05-20 01:46:19 +0200

    Fix database types

--- .postgresql/patches/m131023_165755_initial.php
+++ .postgresql/patches/m131023_165755_initial.php
@@ -27,6 +27,32 @@ class m131023_165755_initial extends Migration
                 ], '');
 
         $this->addPrimaryKey('pk_module_enabled', 'module_enabled', 'module_id');
+
+        // @TODO: This was moved here from queue module
+        $this->createTable('queue', [
+            'id' => $this->primaryKey(),
+            'channel' => $this->string(50)->notNull(),
+            'job' => $this->binary()->notNull(),
+            'pushed_at' => $this->integer()->notNull(),
+            'ttr' => $this->integer()->notNull(),
+            'delay' => $this->integer()->notNull(),
+            'priority' => $this->integer()->unsigned()->notNull()->defaultValue(1024),
+            'reserved_at' => $this->integer(),
+            'attempt' => $this->integer(),
+            'done_at' => $this->integer(),
+        ]);
+
+        // @TODO: This was moved here from queue module
+        $this->createTable('queue_exclusive', [
+            'id' => $this->string(50)->notNull(),
+            'job_message_id' => $this->string(50),
+            'job_status' => $this->smallInteger()->defaultValue(2),
+            'last_update' => $this->timestamp()
+        ]);
+        $this->addPrimaryKey('pk_queue_exclusive', 'queue_exclusive', 'id');
+
+        // @TODO: This was moved here from queue module
+        $this->alterColumn('queue', 'channel', $this->string(50)->notNull());
     }
 
     public function down()
